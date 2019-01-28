1efd57cdd8db49b5b97f47c9f35f8a13b925b479
Kyle Chin <errorcodefive@gmail.com>
2019-01-28 10:29:21 -0500

    Continuing to fix boilerplate and add support for heroku.

--- server.js
+++ server.js
@@ -8,14 +8,17 @@ var app = express();
 require('dotenv').config();
 
 var port_number = process.env.PORT || 3000;
+var dist_path = path.join(__dirname, 'client/dist');
 
-console.log("using: " + __dirname+'/dist');
-app.use(express.static(__dirname+'/dist'));
+console.log("Express using: " + dist_path);
+
+app.use(express.static(dist_path));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
+//Send all other GET requests to the homepage
 app.get('*',  (req,res)=>{
-    res.sendFile(path.resolve(__dirname, '/dist/index.html'));
+    res.sendFile(path.join(__dirname, 'dist/index.html'));
 });
 
 module.exports= app.listen(port_number, function(){
